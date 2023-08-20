cb4d97604832946186997cebd6bd4c02629d7827
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-08-20 08:54:30 -0400

    add in dev test messages

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,13 +4,17 @@
 	import ChatMessage from '../../components/ChatMessage.svelte';
 	import type { AiResponseChunk, Message } from '../../models/chat-models';
 	import { getApiResponse, scrollBottom } from '../../libs/chat-interactions';
+	import { dev } from '$app/environment';
 
 	let messages: Message[] = [
 		{
 			role: 'system',
 			content:
 				"Hello! You're currently speaking with an expert software engineer specializing in web development. I have a wealth of experience in Angular, C#, Svelte, and JavaScript. My purpose here is not only to assist but also to teach and help you understand better. Feel free to ask me anything!"
-		},
+		}
+	];
+
+	let testMessaages: Message[] = [
 		{
 			role: 'user',
 			content:
@@ -22,6 +26,10 @@
 				'Sure! Here\'s a long message with line breaks, code blocks, and various markdown elements:\n\n# Testing Markdown Rendering\n\nHello! This is a test to see how well my web page renders Markdown. Markdown is a lightweight markup language that allows you to easily format text without using HTML tags.\n\n## Line Breaks\n\nThis is a paragraph of text.  \nHere is another line of text.  \nAnd yet another line of text.\n\n## Code Blocks\n\n```\n// This is a code block in JavaScript\nfunction helloWorld() {\n  console.log("Hello, world!");\n}\n```\n\n```python\n# This is a code block in Python\ndef say_hello():\n    print("Hello, world!")\n```\n\n## Headings\n\n### Heading 1\n#### Heading 2\n##### Heading 3\n###### Heading 4\n####### Heading 5 (Invalid, treated as regular text)\n\n## Emphasis and Strong emphasis\n\nThis text is *emphasized* using single asterisks.\nThis text is _emphasized_ using underscores.\n\nThis text is **strongly emphasized** using double asterisks.\nThis text is __strongly emphasized__ using double underscores.\n\n## Lists\n\n### Unordered List\n\n- Item 1\n- Item 2\n- Item 3\n\n### Ordered List\n\n1. First item\n2. Second item\n3. Third item\n\n## Links and Images\n\nHere is a [link](https://www.example.com) to a website.\n\n![Markdown logo](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)\n\n## Blockquotes\n\n> This is a blockquote. It is used to highlight text or quote someone.\n\n## Horizontal Rule\n\n---\n\nThat\'s it for now! I hope my Markdown rendering is working properly and this message looks well-formatted on the web page. Let me know if you have any questions or need further assistance!\n\n'
 		}
 	];
+
+	if (dev) {
+		messages = messages.concat(testMessaages);
+	}
 
 	// This is here to track incomplete data from the buffer. The bugger could give us half the response in one chunk and the other half in another
 	// This is here to help facilitate that partial chunk
