08ec8310f1434fabfa1f8e3e1471b7a5e5868375
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-08-27 13:20:28 -0400

    partially add auth

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+</script>
+
+<div class="dark:bg-slate-900 h-full w-screen">
+	<slot />
+</div>
