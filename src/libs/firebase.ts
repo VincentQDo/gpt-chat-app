c8e9472821710883946e8fd9b8011a74a023b909
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-09-01 09:28:32 -0400

    got the user createto work

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp, type FirebaseApp } from 'firebase/app';
+import { getAnalytics, type Analytics } from 'firebase/analytics';
+import { browser } from '$app/environment';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export let app: FirebaseApp | undefined;
+export let analytics: Analytics;
+if (browser) {
+	app = initializeApp(firebaseConfig);
+	analytics = getAnalytics(app);
+}
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,5 +1,12 @@
 <script>
-	import { getAuth, createUserWithEmailAndPassword,, signInWithEmailAndPassword } from 'firebase/auth';
+	import {
+		getAuth,
+		createUserWithEmailAndPassword,
+		signInWithEmailAndPassword
+	} from 'firebase/auth';
+	import { app } from '../../libs/firebase';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	/**
 	 * @type {string}
@@ -10,19 +17,32 @@
 	 */
 	let password;
 
-	const auth = getAuth();
+	/**
+	 * @type {import("firebase/auth").Auth}
+	 */
+	let auth;
+	onMount(() => {
+		auth = getAuth(app);
+	});
 
 	const signUp = () => {
-		createUserWithEmailAndPassword(auth, email, password);
-	}
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// What do i need to do inside here?
+				console.log(userCredential);
+			})
+			.catch((error) => {
+				// What to do here?
+				console.log(error);
+			});
+	};
 
 	const signIn = () => {
-		const auth = getAuth();
 		signInWithEmailAndPassword(auth, email, password);
-	}
+	};
 </script>
 
 <input type="text" bind:value={email} />
 <input type="text" bind:value={password} />
-<button on:click={() => signUp()}>sign up</button>
-<button on:click={() => signIn()}>sign in</button>
+<button class="" on:click={() => signUp()}>sign up</button>
+<button class="" on:click={() => signIn()}>sign in</button>
