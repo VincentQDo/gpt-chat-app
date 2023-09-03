63370313956c2327a8bf97a9a7ff15d9ee5a269c
Vince <vincentquangdo@gmail.com>
2023-09-03 17:41:15 -0400

    Add node adapter and remove preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,11 @@
 export default {
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('preline/plugin'),
   ],
 }
 
