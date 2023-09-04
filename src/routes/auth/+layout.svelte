32e78fdd764b103104ce1ffcbf99add118533e3a
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-09-04 10:13:41 -0400

    remove vercel analytic

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,9 +1,5 @@
 <script>
 	import '../../app.css';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="dark:bg-slate-900 h-full w-screen">
