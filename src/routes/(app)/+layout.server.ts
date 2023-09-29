ef2bbf6afd3efa6129cff6ef0cdd038accc7e033
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2023-09-29 15:58:49 -0400

    remove auth for now

--- src/routes/(app)/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
-export const load: LayoutServerLoad = ({ locals }) => {
-	if (!locals.user) {
-		console.log(locals);
-		throw redirect(307, '/auth');
-	}
-};
