4bd800263032b4d1912fa0abaa5dbcd0e39beb81
Vince <vincentquangdo@gmail.com>
2023-11-10 12:14:30 -0500

    update to use firebase config file.

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -2,19 +2,20 @@
 import { initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAnalytics, type Analytics } from 'firebase/analytics';
 import { browser } from '$app/environment';
+import env from './firebase-config.json';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+	apiKey: env.VITE_FIREBASE_APIKEY,
+	authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: env.VITE_FIREBASE_APP_ID,
+	measurementId: env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
