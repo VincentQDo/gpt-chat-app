3bc58c83225366c62515f1f65c3ee9571afad34c
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-01-01 22:23:07 -0500

    broken

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -5,8 +5,7 @@
 	import type { Message } from '../../models/chat-models';
 	import {
 		decodeAiResponse,
-		getApiResponse,
-		scrollBottom
+		getApiResponse
 	} from '../../libs/chat-interactions';
 	import { dev } from '$app/environment';
 
@@ -16,7 +15,11 @@
 
 	const sendMessage = async (event: { detail: { input: string } }) => {
 		const input = event.detail.input;
-		messages.push({ role: 'user', content: input });
+		messages.push({
+			senderType: 'user',
+			messageText: input,
+			createdAt: new Date()
+		});
 		messages = [...messages];
 		const response: Response = await getApiResponse(messages);
 		const reader = response.body?.getReader();
@@ -35,7 +38,6 @@
 				messages[messages.length - 1].content = aiReply;
 			}
 			messages = [...messages];
-			scrollBottom(messageContainer);
 			isAiTyping = true;
 		}
 		if (dev) {
@@ -54,12 +56,22 @@
 </script>
 
 <div class="h-full w-screen text-white flex flex-col">
-	<div class="overflow-auto p-4 space-y-4" bind:this={messageContainer}>
-		{#each messages as message, i (i)}
-			{#if message.role !== 'system'}
-				<ChatMessage {message} />
-			{/if}
-		{/each}
+	<div class="flex">
+		<div>
+			<ul>
+				{#each conversations as conversation, i}
+					<li>conversation.title</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="overflow-auto p-4 space-y-4" bind:this={messageContainer}>
+			<ChatMessage message={{ role: 'user', content: 'test message' }} />
+			{#each messages as message, i (i)}
+				{#if message.role !== 'system'}
+					<ChatMessage {message} />
+				{/if}
+			{/each}
+		</div>
 	</div>
 	<ChatInput on:sendMessage={sendMessage} />
 </div>
