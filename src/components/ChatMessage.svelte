f2e33aff71f0a5ad71fac09cab88b0a91e1d4240
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-01-28 18:38:56 +0000

    Bind buttons and messages components

--- src/components/ChatMessage.svelte
+++ src/components/ChatMessage.svelte
@@ -1,27 +1,15 @@
-<script lang="ts">
-	import showdown from 'showdown';
-	import type { Message } from '../models/chat-models';
-
-	export let message: Message;
-
-	const formatMessage = (msgContent: string) => {
-		const converter = new showdown.Converter({
-			noHeaderId: true,
-			tables: true
-		});
-		const htmlOutput = converter.makeHtml(msgContent);
-		return htmlOutput;
-	};
+<script>
+	export let message = '';
+	export let userInitial = '';
 </script>
 
 <div class="flex items-end space-x-2 justify-center">
-	<span class="relative flex shrink-0 overflow-hidden rounded-full h-9 w-9"
-		><span
-			class="flex h-full w-full items-center justify-center rounded-full bg-muted"
+	<span class="relative flex shrink-0 overflow-hidden rounded-full h-9 w-9">
+		<span
+			class="flex h-full w-full items-center justify-center rounded-full bg-muted bg-slate-200"
+			>{userInitial}</span
 		>
-			{message.senderType}
-		</span></span
-	>
-	<p class="text-sm text-white">{message.messageText}</p>
+	</span>
+	<p class="text-sm text-white">{message}</p>
 </div>
 <hr class="my-2 border-gray-700" />
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -69,7 +69,7 @@
 // v0 by Vercel.
 // https://v0.dev/t/oOzJOeoGd9C
 -->
-<div class="grid h-screen grid-cols-[300px_1fr] gap-4 dark:bg-gray-900">
+<div class="grid h-screen grid-cols-[300px_1fr] dark:bg-gray-900">
 	<aside
 		class="border-r border-gray-700 bg-gray-800 transition-colors duration-200"
 	>
@@ -81,18 +81,18 @@
 		</div>
 	</aside>
 	<main class="flex flex-col bg-gray-900">
-		{#each messages as message}
-			<div class="flex-1 overflow-y-auto p-4 space-y-4 text-center">
-				<ChatMessage {message} />
-			</div>
-		{/each}
+		<div class="flex-1 overflow-y-auto p-4 space-y-4 text-center">
+			{#each messages as message}
+				<ChatMessage userInitial="U" message={message.messageText} />
+			{/each}
+		</div>
 		<div class="border-t border-gray-700 p-4 bg-gray-800">
 			<div class="flex items-center space-x-2">
 				<textarea
 					class="flex w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1 min-h-[20px] overflow-auto resize-none text-white bg-gray-700"
 					placeholder="Type your message here."
 				/>
-				<Button>Send Help</Button>
+				<Button>Send</Button>
 			</div>
 		</div>
 	</main>
