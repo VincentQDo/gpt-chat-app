44ab5b03d5af9a23f748908a3cf2b0fe2f2ad291
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-02-05 01:30:51 +0000

    input file

--- ui/src/components/Input.svelte
+++ ui/src/components/Input.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+	import type { HTMLInputTypeAttribute } from 'svelte/elements';
 
-export let name = '';
-export let id = '';
-export let text = '';
-export let userInput = '';
+	export let name = '';
+	export let id = '';
+	export let type: HTMLInputTypeAttribute | undefined | null = 'text';
+	export let userInput = '';
 </script>
 
 <input
-	type="text"
-	name=""
+	{type}
+	{name}
 	class="flex w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-1 min-h-[20px] overflow-auto resize-none text-white bg-gray-700"
-	id=""
+	{id}
+	bind:value={userInput}
 />
